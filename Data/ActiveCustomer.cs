1c36ee9f9328f8660fac12a9f3bcc8c5fe805520
Elizabeth Sanger <libsang@gmail.com>
2016-10-27 09:52:51 -0500

    finished active customer class

--- Data/ActiveCustomer.cs
+++ Data/ActiveCustomer.cs
@@ -1,42 +1,30 @@
 using System;
 using Bangazon.Models;
 
-namespace Bangazon.Data 
-
+namespace Bangazon.Data
 {
 
-public class ActiveCustomer 
+    public class ActiveCustomer
 
-{
-
-    private static ActiveCustomer _instance;
-
-    public static ActiveCustomer instance 
     {
 
-        get {
-            if (_instance == null) {
-                _instance = new ActiveCustomer();
+        private static ActiveCustomer _instance;
+
+        public static ActiveCustomer instance
+        {
+
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ActiveCustomer();
+                }
+                return _instance;
             }
-            return _instance;
         }
-
-        public Customer Customer {get; set;}
-
-    }
+        public Customer Customer { get; set; }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
+
