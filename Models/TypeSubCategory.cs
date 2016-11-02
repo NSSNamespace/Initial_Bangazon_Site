fdf115c0132e93c7360a0c5492242398863e52ff
Elizabeth Sanger <libsang@gmail.com>
2016-11-02 09:19:33 -0500

    initial version of subcategories model created

---
+++ Models/TypeSubCategory.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+/*
+Author: Liz Sanger 
+*/
+
+namespace Bangazon.Models
+{
+    //The ProductTypeSubCategory class represents the ProductTypeSubCategory table in the BangazonContext db
+    public class ProductTypeSubCategory
+    {
+        [Key]
+        public int ProductTypeSubCategory { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+
+        public int ProductTypeId { get; set; }
+    }
+}
