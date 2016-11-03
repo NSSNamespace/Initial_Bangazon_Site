9c8558945e5c924ff13836205e762659fb4f0b93
Elizabeth Sanger <libsang@gmail.com>
2016-11-03 15:38:27 -0500

    product type subcategories fixed, progress on both dropdowns

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    console.log('ready')
     $("#CustomerId").on("change", function (e) {
+        console.log('CustomerId clicked')
         $.ajax({
             url: `/Customers/Activate/${$(this).val()}`,
             method: "POST",
@@ -7,6 +9,19 @@ $(document).ready(function() {
             contentType: "application/json; charset=utf-8"
         }).done(() => {
             location.reload();
+            console.log('hi')
+        });
+    });
+    $("#Product_ProductTypeId").on("change", function (e) {
+        console.log('ProductTypeId clicked')
+        $.ajax({
+            url: `/Customers/Activate/${$(this).val()}`,
+            method: "POST",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8"
+        }).done(() => {
+            location.reload();
+            console.log('hi')
         });
     });
 });
