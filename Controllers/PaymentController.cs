82f5bf9e7d0c85548e143c1de2566a4828b10843
jammy <Jammylaird@yahoo.com>
2016-11-07 08:33:07 -0600

    renamed paymentType

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -42,7 +42,7 @@ namespace Bangazon.Controllers
                 return RedirectToAction ("Cart", new RouteValueDictionary(new {controller = "Order", action = "Cart", Id = paymentType.NewPaymentType.PaymentTypeId}));
             }
             var model = new PaymentTypeView (context);
-            model.NewPaymentTypeView = paymentType.NewPaymentTypeView;
+            model.NewPaymentType = paymentType.NewPaymentType;
 
             return View(model);
         }
--- ViewModels/PaymentTypeViewModel.cs
+++ ViewModels/PaymentTypeViewModel.cs
@@ -6,7 +6,7 @@ namespace Bangazon.ViewModels
 {
     public class PaymentTypeView: BaseViewModel
     {
-        public PaymentType NewPayMentType{get; set;}
+        public PaymentType NewPaymentType{get; set;}
         public PaymentTypeView(BangazonContext ctx) : base(ctx){}
         public PaymentTypeView(){}
     }
