5d5a8539f188c68498806996654fb2f9e17f1b92
Elizabeth Sanger <libsang@gmail.com>
2016-11-08 18:56:36 -0600

    added comments on CreateProductViewModel and OrderViewModel

--- ViewModels/CreateProductViewModel.cs
+++ ViewModels/CreateProductViewModel.cs
@@ -14,36 +14,38 @@ Methods: CreateProductViewModel class custom constructor, which accepts Bangazon
 namespace Bangazon.ViewModels
 {
     //Create CreateProductViewModel class, which inherits from BaseViewModel, and therefore contains the dropdown menu with customer name selected
-  public class CreateProductViewModel : BaseViewModel
-  {
-    public Product Product { get; set; }
-    public List<SelectListItem> ProductTypeId { get; set; }
-    public List<SelectListItem> ProductTypeSubCategoryId { get; set; }
-    //Create custom constructor that accepts current BangazonContext as argument and passes it up inheritance chain to BaseViewModel
-    public CreateProductViewModel(BangazonContext ctx) : base(ctx) 
-        { 
+    public class CreateProductViewModel : BaseViewModel
+    {
+        public Product Product { get; set; }
+        public List<SelectListItem> ProductTypeId { get; set; }
+        public List<SelectListItem> ProductTypeSubCategoryId { get; set; }
+        //Create custom constructor that accepts current BangazonContext as argument and passes it up inheritance chain to BaseViewModel. Includes the logic necessary to populate the product type (aka category) and product type subcategory drop down menus
+        public CreateProductViewModel(BangazonContext ctx) : base(ctx)
+        {
             var context = ctx;
-            this.ProductTypeId = context.ProductType 
+            this.ProductTypeId = context.ProductType
                 .OrderBy(l => l.Label)
                 .AsEnumerable()
                 .Select(li => new SelectListItem
                 {
                     Text = li.Label,
                     Value = li.ProductTypeId.ToString()
-                }).ToList();   
+                }).ToList();
 
-                  this.ProductTypeId.Insert(0, new SelectListItem { 
+            this.ProductTypeId.Insert(0, new SelectListItem
+            {
                 Text = "Please Select a Category",
                 Value = "0"
-            }); 
+            });
 
-        this.ProductTypeSubCategoryId  = new List<SelectListItem>();
+            this.ProductTypeSubCategoryId = new List<SelectListItem>();
 
-        this.ProductTypeSubCategoryId.Insert(0, new SelectListItem{
-          Text = "Choose a Product Category to See Sub-Categories",
-          Value = ""
-        });
-          
+            this.ProductTypeSubCategoryId.Insert(0, new SelectListItem
+            {
+                Text = "Choose a Product Category to See Sub-Categories",
+                Value = ""
+            });
+
         }
     }
 }
